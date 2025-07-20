3163bf71210bb9abc84da1715ea686c6dfc826f8
shackiru <shaquilledputra@gmail.com>
2025-07-20 20:04:30 +0700

    duar

--- resources/views/features/testimonial/index.blade.php
+++ resources/views/features/testimonial/index.blade.php
@@ -103,141 +103,86 @@
         </div>
     </div>
 
-    <div class="testimonial-client mt-24 w-full">
-        <div class="py-24 w-full">
-            <div class="px-8 lg:px-14 xl:px-44 relative">
-                <span
-                    class="text-9xl text-primary opacity-60 absolute z-1 top-[-20px] left-0 lg:left-6 xl:left-36">“</span>
-                <h1 class="text-4xl lg:text-5xl font-semibold z-2">Testimoni <span class="text-primary">Metrokil</span>
-                </h1>
-                <div class="flex gap-3 text-white">
-                    <img src="{{ asset('icons/Iconsax/Svg/All/linear/arrow-right-white.svg') }}" alt=""
-                        width="20">
+            <div class="testimonial-client mt-24 w-full">
+                <div class="py-24 w-full">
+                    <div class="px-8 lg:px-14 xl:px-44 relative">
+                        <span
+                            class="text-9xl text-primary opacity-60 absolute z-1 top-[-20px] left-0 lg:left-6 xl:left-36">“</span>
+                        <h1 class="text-4xl lg:text-5xl font-semibold z-2">Testimoni <span class="text-primary">Metrokil</span>
+                        </h1>
+                        <div class="flex gap-3 text-white">
+                            <img src="{{ asset('icons/Iconsax/Svg/All/linear/arrow-right-white.svg') }}" alt=""
+                                width="20">
+                        </div>
+                    </div>
+                    <div class="testimonials flex mt-4 py-4 gap-6 overflow-x-auto w-full" data-aos="fade-left">
+                        <div
+                            class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] ml-8 lg:ml-14 xl:ml-44 shadow-soft">
+                            <div class="identity">
+                                <h4 class="text-primary font-bold text-xl">Shaquille Ditama Putra</h4>
+                                <p class="text-md opacity-70 font-semibold mt-1">Magang BCA, Ex-PPTI 14</p>
+                            </div>
+                            <div class="content mt-5">
+                                <span class="text-5xl text-primary font-bold">“</span>
+                                <p class="text-lg mt-[-15px] font-semibold">Terimakasih kepada Tim Metrokil, kerjanya
+                                    sangat profesional,
+                                    transparan kepada customer dan penjelasannya detail terkait layanan yang diberikan.
+                                    Sukses selalu Metrokil!</p>
+                            </div>
+                        </div>
+                        <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] shadow-soft">
+                            <div class="identity">
+                                <h4 class="text-primary font-bold text-xl">Aurelia Natasha</h4>
+                                <p class="text-md opacity-70 font-semibold mt-1">Software Engineer, Ex-PPTI 12</p>
+                            </div>
+                            <div class="content mt-5">
+                                <span class="text-5xl text-primary font-bold">“</span>
+                                <p class="text-lg mt-[-15px] font-semibold">Kerjasama dengan Tim Metrokil sangat memuaskan!
+                                    Timnya responsif,
+                                    detail, dan memastikan setiap kebutuhan saya terpenuhi. Metrokil adalah pilihan terbaik
+                                    untuk layanan profesional!</p>
+                            </div>
+                        </div>
+                        <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] shadow-soft">
+                            <div class="identity">
+                                <h4 class="text-primary font-bold text-xl">Jonathan Fajar</h4>
+                                <p class="text-md opacity-70 font-semibold mt-1">Product Manager, Ex-PPTI 13</p>
+                            </div>
+                            <div class="content mt-5">
+                                <span class="text-5xl text-primary font-bold">“</span>
+                                <p class="text-lg mt-[-15px] font-semibold">Metrokil memberikan pengalaman layanan yang
+                                    luar biasa!
+                                    Hasil kerja yang profesional dan tim yang sangat kooperatif. Tidak ragu untuk
+                                    merekomendasikan mereka.</p>
+                            </div>
+                        </div>
+                        <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] shadow-soft">
+                            <div class="identity">
+                                <h4 class="text-primary font-bold text-xl">Stephanie Wijaya</h4>
+                                <p class="text-md opacity-70 font-semibold mt-1">Data Scientist, Ex-PPTI 15</p>
+                            </div>
+                            <div class="content mt-5">
+                                <span class="text-5xl text-primary font-bold">“</span>
+                                <p class="text-lg mt-[-15px] font-semibold">Kerja sama dengan Metrokil sangat mengesankan.
+                                    Pelayanan cepat, transparan, dan sangat profesional. Tim mereka benar-benar tahu cara
+                                    memenuhi ekspektasi pelanggan.</p>
+                            </div>
+                        </div>
+                        <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] mr-44 shadow-soft">
+                            <div class="identity">
+                                <h4 class="text-primary font-bold text-xl">Stephanie Wijaya</h4>
+                                <p class="text-md opacity-70 font-semibold mt-1">Data Scientist, Ex-PPTI 15</p>
+                            </div>
+                            <div class="content mt-5">
+                                <span class="text-5xl text-primary font-bold">“</span>
+                                <p class="text-lg mt-[-15px] font-semibold">Kerja sama dengan Metrokil sangat mengesankan.
+                                    Pelayanan cepat, transparan, dan sangat profesional. Tim mereka benar-benar tahu cara
+                                    memenuhi ekspektasi pelanggan.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="testimonials flex mt-4 py-4 gap-6 overflow-x-auto w-full" data-aos="fade-left">
-                <div
-                    class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] ml-8 lg:ml-14 xl:ml-44 shadow-soft">
-                    <div class="identity">
-                        <h4 class="text-primary font-bold text-xl">Shaquille Ditama Putra</h4>
-                        <p class="text-md opacity-70 font-semibold mt-1">Magang BCA, Ex-PPTI 14</p>
-                    </div>
-                    <div class="content mt-5">
-                        <span class="text-5xl text-primary font-bold">“</span>
-                        <p class="text-lg mt-[-15px] font-semibold">Terimakasih kepada Tim Metrokil, kerjanya
-                            sangat profesional,
-                            transparan kepada customer dan penjelasannya detail terkait layanan yang diberikan.
-                            Sukses selalu Metrokil!</p>
-                    </div>
-                </div>
-                <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] shadow-soft">
-                    <div class="identity">
-                        <h4 class="text-primary font-bold text-xl">Aurelia Natasha</h4>
-                        <p class="text-md opacity-70 font-semibold mt-1">Software Engineer, Ex-PPTI 12</p>
-                    </div>
-                    <div class="content mt-5">
-                        <span class="text-5xl text-primary font-bold">“</span>
-                        <p class="text-lg mt-[-15px] font-semibold">Kerjasama dengan Tim Metrokil sangat memuaskan!
-                            Timnya responsif,
-                            detail, dan memastikan setiap kebutuhan saya terpenuhi. Metrokil adalah pilihan terbaik
-                            untuk layanan profesional!</p>
-                    </div>
-                </div>
-                <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] shadow-soft">
-                    <div class="identity">
-                        <h4 class="text-primary font-bold text-xl">Jonathan Fajar</h4>
-                        <p class="text-md opacity-70 font-semibold mt-1">Product Manager, Ex-PPTI 13</p>
-                    </div>
-                    <div class="content mt-5">
-                        <span class="text-5xl text-primary font-bold">“</span>
-                        <p class="text-lg mt-[-15px] font-semibold">Metrokil memberikan pengalaman layanan yang
-                            luar biasa!
-                            Hasil kerja yang profesional dan tim yang sangat kooperatif. Tidak ragu untuk
-                            merekomendasikan mereka.</p>
-                    </div>
-                </div>
-                <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] shadow-soft">
-                    <div class="identity">
-                        <h4 class="text-primary font-bold text-xl">Stephanie Wijaya</h4>
-                        <p class="text-md opacity-70 font-semibold mt-1">Data Scientist, Ex-PPTI 15</p>
-                    </div>
-                    <div class="content mt-5">
-                        <span class="text-5xl text-primary font-bold">“</span>
-                        <p class="text-lg mt-[-15px] font-semibold">Kerja sama dengan Metrokil sangat mengesankan.
-                            Pelayanan cepat, transparan, dan sangat profesional. Tim mereka benar-benar tahu cara
-                            memenuhi ekspektasi pelanggan.</p>
-                    </div>
-                </div>
-                <div class="testimonial-item bg-white px-8 py-6 rounded-xl min-w-[500px] mr-44 shadow-soft">
-                    <div class="identity">
-                        <h4 class="text-primary font-bold text-xl">Stephanie Wijaya</h4>
-                        <p class="text-md opacity-70 font-semibold mt-1">Data Scientist, Ex-PPTI 15</p>
-                    </div>
-                    <div class="content mt-5">
-                        <span class="text-5xl text-primary font-bold">“</span>
-                        <p class="text-lg mt-[-15px] font-semibold">Kerja sama dengan Metrokil sangat mengesankan.
-                            Pelayanan cepat, transparan, dan sangat profesional. Tim mereka benar-benar tahu cara
-                            memenuhi ekspektasi pelanggan.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="mt-12 lg:mx-14 xl:mx-44 mx-8 mb-20 lg:mb-36" id="gallery">
-        <div class="gallery-header flex justify-between items-center">
-            <h1 class="text-4xl lg:text-5xl font-semibold">Gallery <span class="text-primary">Metrokil</span></h1>
-            <div class="gallery-more flex gap-3">
-                <p class="font-medium text-lg text-primary"><a href="testimonials/galleries">Pelajari selengkapnya</a>
-                </p>
-                <img src="{{ asset('icons/Iconsax/Svg/All/linear/arrow-right-red.svg') }}" alt=""
-                    width="20">
-            </div>
-        </div>
-        <div class="about-detail grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-            <div class="gallery-pictures" data-aos="fade-up">
-                <a href="#">
-                    <div class="gallery-content flex flex-col justify-between text-center">
-                        <img src="{{ asset('images/gallery1.png') }}" alt="">
-                    </div>
-                </a>
-            </div>
-            <div class="gallery-pictures" data-aos="fade-up">
-                <a href="#">
-                    <div class="gallery-content flex flex-col justify-between text-center">
-                        <img src="{{ asset('images/gallery1.png') }}" alt="">
-                    </div>
-                </a>
-            </div>
-            <div class="gallery-pictures" data-aos="fade-up">
-                <a href="#">
-                    <div class="gallery-content flex flex-col justify-between text-center">
-                        <img src="{{ asset('images/gallery1.png') }}" alt="">
-                    </div>
-                </a>
-            </div>
-            <div class="gallery-pictures" data-aos="fade-up">
-                <a href="#">
-                    <div class="gallery-content flex flex-col justify-between text-center">
-                        <img src="{{ asset('images/gallery1.png') }}" alt="">
-                    </div>
-                </a>
-            </div>
-            <div class="gallery-pictures" data-aos="fade-up">
-                <a href="#">
-                    <div class="gallery-content flex flex-col justify-between text-center">
-                        <img src="{{ asset('images/gallery1.png') }}" alt="">
-                    </div>
-                </a>
-            </div>
-            <div class="gallery-pictures" data-aos="fade-up">
-                <a href="#">
-                    <div class="gallery-content flex flex-col justify-between text-center">
-                        <img src="{{ asset('images/gallery1.png') }}" alt="">
-                    </div>
-                </a>
-            </div>
-
         </div>
     </div>
 </x-template>
